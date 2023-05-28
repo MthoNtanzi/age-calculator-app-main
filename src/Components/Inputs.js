829b5009568bcd7633f7e0b86f7e41166e982413
Mtho <mthontanzi99@gmail.com>
2023-05-28 20:47:06 +0200

    add function to calculate date

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -4,6 +4,9 @@ import arrow from '../images/icon-arrow.svg';
 
 export default function Inputs() {
   const [inputDayVal, setInputDayVal] = useState("");
+  const [inputMonthVal, setInputMonthVal] = useState("");
+  const [inputYearVal, setInputYearVal] = useState("");
+  const [theDate, setTheDate] = useState("");
 
   const handleDayChange = (e) =>{
     const day = e.target.value;
@@ -11,24 +14,50 @@ export default function Inputs() {
     console.log(day);
   }
 
-  const [inputMonthVal, setInputMonthVal] = useState("");
-
   const handleMonthChange = (e) =>{
     const month = e.target.value;
     setInputMonthVal(month);
     console.log(month);
   }
-  const [inputYearVal, setInputYearVal] = useState("");
-
+ 
   const handleYearChange = (e) =>{
     const year = e.target.value;
     setInputYearVal(year);
     console.log(year);
   }
 
-  const current = new Date();
-  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-  console.log(userDate)
+  const giveDate = () => {
+    const dateString = `${inputYearVal}-${inputMonthVal}-${inputDayVal}`;
+    setTheDate(dateString);
+    console.log(theDate);
+
+
+    const timestamp = Date.parse(dateString);
+    const newDate = new Date(timestamp);
+    console.log(newDate);
+
+
+    const today = new Date(),
+    
+    dob = new Date(newDate),
+    //difference in milliseconds
+    diff = today.getTime() - dob.getTime(),
+    //convert milliseconds into years
+    years = Math.floor(diff / 31556736000),
+    //1 day has 86400000 milliseconds
+    days_diff= Math.floor((diff % 31556736000) / 86400000),
+    //1 month has 30.4167 days
+    months = Math.floor(days_diff / 30.4167),
+    days = Math.floor(days_diff % 30.4167);
+   
+  //  console.log(`${years} years ${months} months ${days} days`);
+   let trueAge = {"year": years, "month":months, "day": days};
+  //  return trueAge;
+  
+   console.log(trueAge);
+    
+  }
+
   return (
     <>
       <div className='inputFields'>
@@ -75,7 +104,7 @@ export default function Inputs() {
       </div>
       {/* Calculate button */}
       <div className='buttonField'>
-          <img src={arrow} className='iconArrow' alt='Calculate Arrow' />
+          <img onClick={giveDate} src={arrow} className='iconArrow' alt='Calculate Arrow' />
         </div>
     </>
   )
--- src/Components/Outputs.js
+++ src/Components/Outputs.js
@@ -4,9 +4,9 @@ import './Output.css'
 export default function Outputs() {
   return (
     <div>
-        <p className='outputValue'><span className='outputNumber'>--</span>years</p>
-        <p className='outputValue'><span className='outputNumber'>--</span>months</p>
-        <p className='outputValue'><span className='outputNumber'>--</span>days</p>
+        <p className='outputValue'><span id='yearField' className='outputNumber'>--</span>years</p>
+        <p className='outputValue'><span id='monthField' className='outputNumber'>--</span>months</p>
+        <p className='outputValue'><span id='dayField' className='outputNumber'>--</span>days</p>
     </div>
   )
 }
